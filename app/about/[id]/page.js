de18621e2d9065a31c45420023aaf047e8ffece2
rayanmcheik <rayan.m.mcheik@gmail.com>
2025-12-02 15:00:22 +0200

    done

--- app/about/[id]/page.js
+++ app/about/[id]/page.js
@@ -21,33 +21,84 @@ export default function AboutDetails() {
             const data = await getData();
             const arr = data.data;
             const dataToUse = arr.find((mov) => String(mov.id) === String(id));
-            const projectIndex = arr.findIndex(
-                (mov) => String(mov.id) === String(id)
-            );
+            const projectIndex = arr.findIndex((mov) => String(mov.id) === String(id));
             setProject(dataToUse);
             setIndex(projectIndex);
         }
-
         load();
     }, [id]);
 
     if (!project) {
-        return <div className='flex items-start justify-center w-full bg-black h-lvh'><h1 className='text-3xl font-bold text-white'>Loading...</h1></div>;
+        return (
+            <div className='flex items-start justify-center w-full bg-black h-lvh'>
+                <h1 className='text-3xl font-bold text-white'>Loading...</h1>
+            </div>
+        );
     }
 
     return (
         <div className="w-full h-full bg-black">
             <div className="container w-full h-full px-0 pt-20 pb-12 mx-auto md:pt-20 md:pb-52">
-                <div className="flex flex-col items-center justify-center">
-                    <h1 className="mb-4 text-3xl font-bold text-white">{project.title}</h1>
-                    
-                    <img
-                        src={project.image}
-                        className="w-full max-w-3xl mb-8 rounded-lg"
-                    />
-                    <p className="text-xl text-white">{project.description || "No description available"}</p>
+                <div className="flex flex-col items-center justify-center text-white text-lg gap-y-4">
+
+                    <h1 className="mb-4 text-3xl font-bold">{project.title}</h1>
+
+                    {project.image && (
+                        <img
+                            src={project.image}
+                            className="w-full mb-8 rounded-lg"
+                            alt={project.title}
+                        />
+                    )}
+
+
+
+                    {project.client && (
+                        <p><span className="font-bold">Client:</span> {project.client}</p>
+                    )}
+
+                    {project.directors && (
+                        <p><span className="font-bold">Directors:</span> {project.directors}</p>
+                    )}
+
+                    {project.cinematographer && (
+                        <p><span className="font-bold">Cinematographer:</span> {project.cinematographer}</p>
+                    )}
+
+                    {project.colorist && (
+                        <p><span className="font-bold">Colorist:</span> {project.colorist}</p>
+                    )}
+
+                    {project.agency && (
+                        <p><span className="font-bold">Agency:</span> {project.agency}</p>
+                    )}
+
+                    {project.date && (
+                        <p><span className="font-bold">Date:</span> {project.date}</p>
+                    )}
+                    {project.vimeo_id && (
+                        <div className="w-full flex justify-center">
+                            <iframe
+                                src={`https://player.vimeo.com/video/${project.vimeo_id}`}
+                                className="w-full h-[80vh] md:h-[50vh] rounded-lg "
+                                frameBorder="0"
+                                allow="autoplay; fullscreen"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    )}
+
+                    {project.description && (
+                        <p>{project.description}</p>
+                    )}
+
+                    {project.text && (
+                        <p>{project.text}</p>
+                    )}
                 </div>
+
             </div>
+
         </div>
     );
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,9 @@
 'use client';
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Autoplay, EffectCoverflow } from "swiper/modules";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import axios from "axios";
 import "swiper/css";
-import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
@@ -26,13 +25,9 @@ export default function AboutPage() {
     }, []);
 
     return (
-
-
         <div className="w-full h-full bg-black">
             <div className="container w-full h-full px-0 pt-20 pb-12 mx-auto md:pt-20 md:pb-52">
-
-
-                <div className="w-full h-[400px] ">
+                <div className="w-full h-[400px]">
                     <Swiper
                         slidesPerView={3}
                         spaceBetween={20}
@@ -52,35 +47,29 @@ export default function AboutPage() {
                                     key={project.id}
                                     className="relative flex items-center justify-center"
                                 >
+                                    <a href={`/about/${project.id}`} className="absolute inset-0 z-10">
+                                        <span className="sr-only">Go to {project.title}</span>
+                                    </a>
                                     <img
-                                        src={project.image}
+                                        src={project.image }
                                         alt={project.title || "Project Image"}
                                         className="object-cover w-full h-[400px] rounded-lg"
                                     />
-                                    <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/40">
+                                    <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black/40 z-0">
                                         <h2 className="px-4 text-2xl font-bold text-center text-white">
                                             {project.title}
                                         </h2>
                                     </div>
                                     <a
-                                        href={`/about/${project.id}`}
-                                        className="absolute px-4 py-2 text-xl font-semibold text-white transition rounded-lg bottom-2 right-2 "
-                                    >
-                                        See More
-                                    </a>
+                                        href={`/about/${project.id}`}                      
+                                     className="absolute z-50 px-4 py-2 text-xl font-semibold text-white transition rounded-lg bottom-2 right-2 hover:bg-red-500" >
+                                         See More 
+                                         </a>
                                 </SwiperSlide>
-
                             ))}
-                        <div className="w-10 h-10 text-red-500! swiper-button-prev"></div>
-                        <div className="w-10 h-10 text-red-500! swiper-button-next"></div>
+                        <div className="swiper-button-prev text-red-500!"></div>
+                        <div className="swiper-button-next text-red-500!"></div>
                     </Swiper>
-
-
-                </div>
-
-
-                <div className="pt-52 ">
-
                 </div>
             </div>
         </div>
