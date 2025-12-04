1356d19d5de3356bf83838aad4ef0a8a18b5e0a1
rayanmcheik <rayan.m.mcheik@gmail.com>
2025-12-04 15:33:33 +0200

    DONE

--- app/ourwork/Card.tsx
+++ app/ourwork/Card.tsx
@@ -12,7 +12,7 @@ const Card: React.FC<CardProps> = ({ title, description, img }) => {
     <div className="bg-gray-800 rounded-xl overflow-hidden shadow-lg hover:scale-[1.02] transition cursor-pointer">
       <img src={img} alt={title} className="w-full h-56 object-cover" />
       <div className="p-5">
-        <h2 className="text-2xl font-bold mb-2">{title}</h2>
+        <h2 className="text-2xl font-bold mb-2 text-cyan-500">{title}</h2>
         <p className="text-gray-300 text-lg">{description}</p>
       </div>
     </div>
