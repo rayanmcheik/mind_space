026d1ae347cffdda5050496ec6a0a56e57c13263
rayanmcheik <rayan.m.mcheik@gmail.com>
2025-12-10 15:40:03 +0200

    done

--- app/ourclient/Card.tsx
+++ app/ourclient/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ image, name, comment, handle }) => {
   return (
-    <div className="flex flex-col max-w-sm gap-3 p-5 bg-white shadow-md cursor-pointer h-44 rounded-2xl">
+    <div className="flex flex-col gap-3 p-5 bg-white shadow-md cursor-pointer h-44 rounded-2xl">
       <p className="text-sm leading-relaxed text-black">{comment}</p>
       <div className="flex items-center gap-3">
         <img src={image} alt={name} className="object-cover w-12 h-12 rounded-full" />
