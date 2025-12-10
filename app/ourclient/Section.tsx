804e255bc18a67edf4771d9925c83638feb1e633
rayanmcheik <rayan.m.mcheik@gmail.com>
2025-12-10 15:35:59 +0200

    done

--- app/ourclient/Section.tsx
+++ app/ourclient/Section.tsx
@@ -7,7 +7,7 @@ const Section = () => {
         We have worked with thousands of amazing people
       </h1>
 
-      <div className="grid w-full max-w-6xl grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid w-full grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <Card
           image="/man.jpg"
           name="David West"
